f65f7a67607a3579aea425986d14a27fcaf9861e
Lisa Caton <lisacaton01@gmail.com>
2018-06-21 10:39:32 -0400

    pre-video:unique post and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,12 @@ posts = Post.all
   )
 end
 
+puts"#{Post.count}"
+Post.find_or_create_by!(title: 'Crabtree', body:'Crabtree Eveyln')
+puts"#{Post.count}"
+
+
+
 puts"Seed finished"
 puts"#{Post.count} posts created"
 puts"#{Comment.count} comments created"

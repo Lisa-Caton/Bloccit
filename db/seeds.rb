c77e1e8567ca88f0a48e3756433ac36e91362818
Lisa Caton <lisacaton01@gmail.com>
2018-06-21 10:40:30 -0400

    pre-video:unique post and comment- with comment now

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ puts"#{Post.count}"
 Post.find_or_create_by!(title: 'Crabtree', body:'Crabtree Eveyln')
 puts"#{Post.count}"
 
-
+Comment.find_or_create_by!(post_id: 101, body:'Summer time scents!')
 
 puts"Seed finished"
 puts"#{Post.count} posts created"

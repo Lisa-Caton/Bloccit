e5ef9b5defacd33d5271e1f96def1664f461a79a
Lisa Caton <lisacaton01@gmail.com>
2018-06-25 12:35:53 -0400

    CRUD post and their path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
-  get 'welcome/faq'
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/edit'
+  # can replace all of these generated routes with a more succinct syntax with
+  # resources :posts
+  # we call the resources method and pass it a Symbol
+  resources :posts
+
+  # get 'welcome/index'
+  # get 'welcome/about'
+
+  # get 'welcome/contact'
+  # get 'welcome/faq'
+
+  # more succinct syntax
+  # we remove get "welcome/index" because we've declared the index view as the root view
+  #We also modify the about route to allow users to visit /about, rather than /welcome/about
+  get 'about' => 'welcome#about'
+
   root 'welcome#index'
 end

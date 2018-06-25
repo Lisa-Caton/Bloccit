cb6dd7585e2066e3be647c88c9748fc2729bca4a
Lisa Caton <lisacaton01@gmail.com>
2018-06-25 16:28:22 -0400

    exercise: modulus for post = SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-     # we declare an instance variable @posts and assign it a collection of Post objects using the all method provided by ActiveRecord. 
+     # we declare an instance variable @posts and assign it a collection of Post objects using the all method provided by ActiveRecord.
      # all returns a collection of Post objects.
+     @posts.each_with_index do |post, id|
+       if id % 5 == 0
+         post.title = "SPAM"
+       end
+     end
   end
 
   def show

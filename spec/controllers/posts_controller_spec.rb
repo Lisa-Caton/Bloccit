25b1887d8f81ab34be78a310cd05a9bc28de82bb
Lisa Caton <lisacaton01@gmail.com>
2018-07-10 09:57:42 -0400

    Slack help - rspec failing

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe PostsController, type: :controller do
 let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 # we create a post and assign it to my_post using let.
 # we use RandomData to give my_post a random title and body.
+# let(:my_post) do 
+#   Post.create(
+#     title: RandomData.random_sentence,
+#     body: RandomData.random_paragraph
+#   )
+# end
 
   describe "GET #index" do
     it "returns http success" do
@@ -99,11 +105,22 @@ let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData
 
   end
 
-
-  #
   # describe "GET #edit" do
   #   it "returns http success" do
-  #     get :edit
+  #     get :edit, params: {id: my_posts.id}
   #     expect(response).to have_http_status(:success)
+  #   end
+  #   it "renders the #edit view" do
+  #     get :edit, params: {id: my_post.id}
+  #     expect(response).to render_template :edit
+  #   end
+  #   it "assigns the post to be updated to @post" do
+  #     get :edit, params: {id: my_post.id}
+
+  #     post_instance = assigns(:post)
+
+  #     expect(post_instance.id).to eq my_post.id
+  #     expect(post_instance.title).to eq my_post.title
+  #     expect(post_instance.body).to eq my_post.body
   #   end
   # end

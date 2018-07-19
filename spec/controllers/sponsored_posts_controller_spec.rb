28d786d121eecfa464eb1a4b02ea6b4d652886ec
Lisa Caton <lisacaton01@gmail.com>
2018-07-19 13:05:42 -0400

    1st spec on controller, GET show, error still

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe SponsoredPostsController, type: :controller do
 
+   let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+   let(:sponsored_posts) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, params: { topic_id: my_topic.id }
       expect(response).to have_http_status(:success)
     end
   end

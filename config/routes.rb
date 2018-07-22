c15986b6e000ca0a2ba1c37b7e9261eb13259d99
Lisa Caton <lisacaton01@gmail.com>
2018-07-21 20:17:34 -0400

    sign-up Slack help

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
     resources :sponsored_posts, except: [:index]
    end
 
+
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
+
   resources :advertisements
   resources :questions
 

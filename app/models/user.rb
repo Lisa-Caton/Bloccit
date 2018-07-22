dbd61807af8c650bbbe6f0417ad3faa93d5b7ab4
Lisa Caton <lisacaton01@gmail.com>
2018-07-22 12:28:14 -0400

    rearranged error msg order

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,14 @@ class User < ApplicationRecord
   # before_save { self.name = name.downcase.gsub(/\b\w/, &:upcase) if name.present? }
 
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-    validates :password, presence: true, length: { minimum: 6 }, if: -> { "password_digest.nil?" }
-    validates :password, length: { minimum: 6 }, allow_blank: true
+    
     validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
+
+    validates :password, presence: true, length: { minimum: 6 }, if: -> { "password_digest.nil?" }
+    validates :password, length: { minimum: 6 }, allow_blank: true
 
     has_secure_password
 
